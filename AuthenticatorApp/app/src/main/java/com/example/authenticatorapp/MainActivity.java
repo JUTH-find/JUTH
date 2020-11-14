3b2dc259d1fb90ff16649ae36a3af7275daa99ff
titichayajojo <titichaya_love@hotmail.com>
2020-11-14 16:48:20 +0700

    tabs layout

--- AuthenticatorApp/app/src/main/java/com/example/authenticatorapp/MainActivity.java
+++ AuthenticatorApp/app/src/main/java/com/example/authenticatorapp/MainActivity.java
@@ -5,23 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("J U T H !");
+
+        myViewPager = (ViewPager)findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 
-    public void logout(View view){
-        FirebaseAuth.getInstance().signOut(); //logout
-        startActivity(new Intent(getApplicationContext() , Login.class));
-        finish();
-    }
-    public void loadProfile(View view){
-        startActivity(new Intent(getApplicationContext(),Profile.class));
-    }
+
+
 }
