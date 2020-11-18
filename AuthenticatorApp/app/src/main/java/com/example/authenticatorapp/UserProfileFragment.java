17565124000c81883b8657c45499b5f4570f14e2
JOJO <titichaya_love@hotmail.com>
2020-11-18 23:30:46 +0700

    Marge JUTH

--- AuthenticatorApp/app/src/main/java/com/example/authenticatorapp/UserProfileFragment.java
+++ AuthenticatorApp/app/src/main/java/com/example/authenticatorapp/UserProfileFragment.java
@@ -111,6 +111,7 @@ public class UserProfileFragment extends Fragment {
         storageReference = FirebaseStorage.getInstance().getReference();
         StorageReference profileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
         profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
             @Override
             public void onSuccess(Uri uri) {
                 Picasso.get().load(uri).into(profileImage);
