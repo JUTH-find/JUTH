1ca7d06ffa819497de263b16efb6a49f5fde6675
JOJO <titichaya_love@hotmail.com>
2020-11-18 22:49:50 +0700

    Marge Chat and Login

--- AuthenticatorApp/app/src/main/java/com/example/authenticatorapp/ChatsFragment.java
+++ AuthenticatorApp/app/src/main/java/com/example/authenticatorapp/ChatsFragment.java
@@ -2,18 +2,40 @@ package com.example.authenticatorapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ChatsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChatsFragment extends Fragment {
+public class ChatsFragment extends Fragment implements View.OnClickListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +45,18 @@ public class ChatsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference messagedb,userRef;
+    MessageAdapter messageAdapter;
+    FirebaseAuth fAuth;
+    List<Message> messages;
+    String user_id;
+
+    RecyclerView rvMessage;
+    EditText etMessage;
+    Button btSend;
 
     public ChatsFragment() {
         // Required empty public constructor
@@ -53,12 +87,121 @@ public class ChatsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+    }
+
+    public void onClick(View view){
+        if(!TextUtils.isEmpty(etMessage.getText().toString())){
+            Message message = new Message(etMessage.getText().toString(), AllMethods.name);
+            etMessage.setText("");
+            messagedb.push().setValue(message);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        fAuth = FirebaseAuth.getInstance();
+        View v = inflater.inflate(R.layout.fragment_chats, container, false);
+        auth = FirebaseAuth.getInstance();
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        database = FirebaseDatabase.getInstance();
+        user_id = fAuth.getCurrentUser().getUid();
+
+        rvMessage = (RecyclerView)v.findViewById(R.id.rvMessage);
+        etMessage = (EditText)v.findViewById(R.id.messageBox);
+        btSend = (Button)v.findViewById(R.id.sendButton);
+        btSend.setOnClickListener(this);
+        messages = new ArrayList<>();
+        return v;
+    }
+
+
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        database.getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dss) {
+                AllMethods.name = dss.child(user_id).child("Name").getValue().toString();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        messagedb = database.getReference("message");
+        messagedb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dss, @Nullable String previousChildName) {
+                Message message = dss.getValue(Message.class);
+                message.setKey(dss.getKey());
+                messages.add(message);
+                displayMessages(messages);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dss, @Nullable String previousChildName) {
+                Message message = dss.getValue(Message.class);
+                message.setKey(dss.getKey());
+
+                List<Message> newMessages = new ArrayList<Message>();
+
+                for(Message m: messages){
+                    if(m.getKey().equals(message.getKey())){
+                        newMessages.add(message);
+                    }
+                    else{
+                        newMessages.add(m);
+                    }
+                }
+                messages = newMessages;
+
+                displayMessages(messages);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dss) {
+                Message message = dss.getValue(Message.class);
+                message.setKey(dss.getKey());
+
+                List<Message> newMessages = new ArrayList<Message>();
+
+                for(Message m: messages){
+                    if(!m.getKey().equals(message.getKey())){
+                        newMessages.add(m);
+                    }
+                }
+                messages = newMessages;
+                displayMessages(messages);
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dss, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void onResume(){
+        super.onResume();
+        messages = new ArrayList<>();
+    }
+
+    private void displayMessages(List<Message> messages) {
+        rvMessage.setLayoutManager(new LinearLayoutManager(getContext()));
+        messageAdapter = new MessageAdapter(getContext(), messages, messagedb);
+        rvMessage.setAdapter(messageAdapter);
     }
 }
